7143d3b2bd7e0350eab5f59b9e479c4ba0384f5f
Manjot <physicist11102003@gmail.com>
2024-10-27 23:34:16 +0530

    created models

---
+++ src/model/User.ts
@@ -0,0 +1,38 @@
+import mongoose , {Schema , Document } from "mongoose";
+
+export interface Message extends Document {
+content: string;
+createdAt: Date;
+}
+
+const messageSchema: Schema<Message> = new Schema({
+content: { type: String, required: true },
+createdAt: { type: Date, 
+    default: Date.now ,
+    required: true },
+});
+
+export interface User extends Document {
+username: string;
+email: string;
+password: string;
+verifycode:string;
+verifyCodeExpiry:Date;
+isAcceptingMessage:boolean;
+messages: Message[];
+isVerified:boolean;
+}
+
+const userSchema: Schema<User> = new Schema({
+username: { type: String, required: true,
+    trim:true,unique:true },
+email: { type: String, required: [true, 'Email is required'],
+    trim:true,unique:true,match: [/.+\@.+\..+/, 'Please fill a valid email address'] },
+password: { type: String, required: true },
+verifycode:{type:String,required:true},
+verifyCodeExpiry:{type:Date,required:true},
+isAcceptingMessage:{type:Boolean,required:true},            
+isVerified:{type:Boolean,required:true,default:false},
+messages: [messageSchema],
+})
+
